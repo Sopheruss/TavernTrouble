cc5c2058f29b4c3a00813dc3da820800faf1d39f
Sophie <sophie_moosmann@web.de>
2024-05-14 14:52:52 +0200

    Just Animation, no movement
    TODO:
    - Resolution fixen
    - Animation auf Movement anpassen

---
+++ SoftwareProjekt2024/AnimationManager.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SoftwareProjekt2024
+{
+    internal class AnimationManager
+    {
+        int numFrames;
+        int numColumns;
+        Vector2 size;
+
+        int counter;
+        int activeFrame;
+        int interval;
+
+        int rowPos;
+        int colPos;
+
+        public AnimationManager(int numFrames, int numColumns, Vector2 size) {
+            this.numFrames = numFrames;
+            this.numColumns = numColumns;
+            this.size = size;
+
+            counter = 0;
+            activeFrame = 0;
+            interval = 30; //30 Frames Intervall 
+
+            rowPos = 0;
+            colPos = 0;
+        }    
+
+        public void Update()
+        {
+            //couter for animation 
+            counter++;
+            if (counter > interval) //animation changes every 30 frames 
+            {
+                counter = 0; //counter reset 
+                NextFrame();
+            }
+        }
+
+        private void NextFrame()
+        {
+            activeFrame++;
+            colPos++;
+            if(activeFrame >= numFrames) //reset active frames 
+            {
+                ResetAnimation();
+            }
+
+            if(colPos >= numColumns) //reset columns
+            {
+                colPos = 0;
+                rowPos++;
+            }
+        }
+
+        private void ResetAnimation()
+        {
+            activeFrame = 0;
+            colPos = 0;
+            rowPos = 0;
+        }
+
+        public Rectangle GetFrame()
+        {
+            return new Rectangle(
+                colPos * (int)size.X, 
+                rowPos * (int)size.Y, 
+                (int)size.X, 
+                (int)size.Y);
+        }
+    }
+}
--- SoftwareProjekt2024/Game1.cs
+++ SoftwareProjekt2024/Game1.cs
@@ -9,21 +9,31 @@ namespace SoftwareProjekt2024
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Texture2D _ogerCookSpritesheet;
+        int screenWidth = 1920;
+        int screenHeight = 1080;
 
-        int counter;
-        int activeFrame;
-        int numFrames; 
+        Texture2D _ogerCookSpritesheet;
+        Vector2 _ogerPosition;
+        float _ogerSpeed; 
+
+        AnimationManager _animationManager;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            /*this._graphics.PreferredBackBufferWidth = screenWidth;
+            this._graphics.PreferredBackBufferHeight = screenHeight;
+
+            this._graphics.IsFullScreen = true;*/
         }
 
         protected override void Initialize()
         {
+            _ogerPosition = new Vector2(_graphics.PreferredBackBufferWidth/2, _graphics.PreferredBackBufferHeight/2);
+            _ogerSpeed = 300f; 
             
             base.Initialize();
         }
@@ -34,10 +44,7 @@ namespace SoftwareProjekt2024
 
             _ogerCookSpritesheet = Content.Load<Texture2D>("Models/oger_cook_spritesheet_lowRes");
 
-            activeFrame = 0;
-            numFrames = 4;
-            counter = 0;
-
+            _animationManager = new(16, 4, new Vector2(19, 32));
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,17 +52,35 @@ namespace SoftwareProjekt2024
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            counter++;
-            if(counter > 29) //animation changes every 30 frames 
-            {
-                counter = 0; //counter reset 
-                activeFrame++; //active frame to next Frame 
+            _animationManager.Update();
+            
 
-                if (activeFrame == numFrames) //reset active frame
-                {
-                    activeFrame = 0;
-                }
+            //Movement in Porgess 
+
+            /*//line-by-line analysis of code 
+            var kstate = Keyboard.GetState();
+
+            //fetches current keyvoard state, stores variables in kstate 
+            if (kstate.IsKeyDown(Keys.Up))
+            {
+                //checks if up buton is pressed; *gameTime... moving regardesless of framerate
+                _ogerPosition.Y -= _ogerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
             }
+
+            if (kstate.IsKeyDown(Keys.Down))
+            {
+                _ogerPosition.Y += _ogerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (kstate.IsKeyDown(Keys.Left))
+            {
+                _ogerPosition.X -= _ogerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (kstate.IsKeyDown(Keys.Right))
+            {
+                _ogerPosition.X += _ogerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }*/ 
 
             base.Update(gameTime);
         }
@@ -67,12 +92,23 @@ namespace SoftwareProjekt2024
             _spriteBatch.Begin();
 
             _spriteBatch.Draw(
+                _ogerCookSpritesheet, //Texture 
+                _ogerPosition, //Position
+                _animationManager.GetFrame(), //source rectangle (frame); HARD CODED!!!!
+                Color.White, //color
+                0f, //rotation 
+                new Vector2(19/2,32/2), //HARD CODED!!!,  ist da, dass Textur richtig liegt 
+                8f, //scale
+                SpriteEffects.None, //effects 
+                0f); //layer depth 
+
+            /*_spriteBatch.Draw(
                 _ogerCookSpritesheet,
                 new Rectangle(100, 100, 100, 200),
-                new Rectangle(activeFrame * 19, 0, 19, 32), //frame rectangle -> must adjust 
-                Color.White);
+                _animationManager.GetFrame(),
+                Color.White);*/
             
-
+            
             _spriteBatch.End();
 
             base.Draw(gameTime);
