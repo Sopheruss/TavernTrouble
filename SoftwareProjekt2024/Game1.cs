f518eabea58eaf79fcf7e4d7d47ea548324b44f9
Leon Brunke <lanceplayer15@gmail.com>
2024-05-17 09:43:46 +0200

    Test for perspective

--- SoftwareProjekt2024/Game1.cs
+++ SoftwareProjekt2024/Game1.cs
@@ -5,12 +5,44 @@ using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Renderers;
 using MonoGame.Extended;
 using MonoGame.Extended.ViewportAdapters;
+using System.Collections.Generic;
+using System;
 namespace SoftwareProjekt2024
 {
     public class Game1 : Game
     {
-        Texture2D cookTexture;
-        Vector2 ballPosition;
+        public class TestTexture : IComparable<TestTexture>
+        {
+            public Texture2D texture;
+            public Vector2 position;
+
+            public void draw(SpriteBatch _spriteBatch)
+            {
+                _spriteBatch.Draw(texture,
+                position,
+                null,
+                Color.White,
+                0f,
+                new Vector2(texture.Width / 2, texture.Height / 2),
+                0.2f,
+                SpriteEffects.None,
+                0f);
+            }
+
+            public int CompareTo(TestTexture other)
+            {
+                if (this.position.Y < other.position.Y) return -1;
+                if (this.position.Y == other.position.Y) return 0;
+                return 1;
+            }
+        }
+
+        SortedSet<TestTexture> sortedTextures;
+        TestTexture texture1 = new TestTexture();
+        TestTexture texture2 = new TestTexture();
+        TestTexture texture3 = new TestTexture();
+        
+
         float ballSpeed;
 
         private GraphicsDeviceManager _graphics;
@@ -39,7 +71,11 @@ namespace SoftwareProjekt2024
         protected override void Initialize()
         {
             //starting position centered based on screen dimensions 
-            ballPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+
+            texture1.position = new Vector2(800, 540);
+            texture2.position = new Vector2(900, 500);
+            texture3.position = new Vector2(1000, 600);
+
             ballSpeed = 300f;
             var viewportadapter = new BoxingViewportAdapter(Window, GraphicsDevice, 800, 600);
             _camera = new OrthographicCamera(viewportadapter);
@@ -52,7 +88,10 @@ namespace SoftwareProjekt2024
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             _tiledMap = Content.Load<TiledMap>("samplemap");
             _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
-            cookTexture = Content.Load<Texture2D>("Oger_Koch");
+            texture1.texture = Content.Load<Texture2D>("Oger_Koch");
+            texture2.texture = Content.Load<Texture2D>("Oger_Koch");
+            texture3.texture = Content.Load<Texture2D>("Oger_Koch");
+            
         }
 
 
@@ -91,13 +130,16 @@ namespace SoftwareProjekt2024
             var speed = 200;
             var seconds = gameTime.GetElapsedSeconds();
             var movementDirection = GetMovementDirection();
-            _cameraPosition += speed * movementDirection * seconds;
+            texture1.position += speed * movementDirection * seconds;
         }
 
 
         protected override void Update(GameTime gameTime)
         {
-
+            sortedTextures = new SortedSet<TestTexture>();
+            sortedTextures.Add(texture1);
+            sortedTextures.Add(texture2);
+            sortedTextures.Add(texture3);
             _tiledMapRenderer.Update(gameTime);
 
             MoveCamera(gameTime);
@@ -167,15 +209,12 @@ namespace SoftwareProjekt2024
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
             //centers correctly (calculating image into center)
-            _spriteBatch.Draw(cookTexture,
-                ballPosition,
-                null,
-                Color.White,
-                0f,
-                new Vector2(cookTexture.Width / 2, cookTexture.Height / 2),
-                0.2f,
-                SpriteEffects.None,
-                0f);
+            
+            foreach(TestTexture texture in sortedTextures)
+            {
+                texture.draw(_spriteBatch);
+            }
+
             _spriteBatch.End();
 
             base.Draw(gameTime);
