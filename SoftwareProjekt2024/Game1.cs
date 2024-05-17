19914693ecbad2fbac5e6525b047eaf850e7499f
Sophie <sophie_moosmann@web.de>
2024-05-17 10:50:05 +0200

    Class Structure

--- SoftwareProjekt2024/Game1.cs
+++ SoftwareProjekt2024/Game1.cs
@@ -1,95 +1,95 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SoftwareProjekt2024.Components;
 using SoftwareProjekt2024.Managers;
 using SoftwareProjekt2024.SpriteClasses;
 using System.Diagnostics;
 
-namespace SoftwareProjekt2024
+namespace SoftwareProjekt2024;
+
+public class Game1 : Game
 {
-    public class Game1 : Game
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    //it is possible to initialize a List of Sprites!!!
+    Player ogerCook; 
+
+    int screenWidth = 1920;
+    int screenHeight = 1080;
+
+    int midScreenWidth;
+    int midScreenHeight;
+
+    AnimationManager _animationManager;
+
+    public Game1()
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
 
-        //it is possible to initialize a List of Sprites!!!
-        Player ogerCook; 
+        this._graphics.PreferredBackBufferWidth = screenWidth;
+        this._graphics.PreferredBackBufferHeight = screenHeight;
 
-        int screenWidth = 1920;
-        int screenHeight = 1080;
+        //this._graphics.IsFullScreen = true;
+    }
 
-        int midScreenWidth;
-        int midScreenHeight;
+    protected override void Initialize()
+    {
+        //to make using calc for middel of Screen shorter 
+        midScreenWidth = _graphics.PreferredBackBufferWidth / 2;
+        midScreenHeight = _graphics.PreferredBackBufferHeight / 2;
 
-        AnimationManager _animationManager;
+        base.Initialize();
+    }
 
-        public Game1()
-        {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            this._graphics.PreferredBackBufferWidth = screenWidth;
-            this._graphics.PreferredBackBufferHeight = screenHeight;
+        //constructing new Animation with 16 Frames in 4 Rows 
+        _animationManager = new(4, 4, new Vector2(19, 32));
 
-            //this._graphics.IsFullScreen = true;
-        }
+        //local implementation, cuz accec to texture via Sprite class 
+        Texture2D _ogerCookSpritesheet = Content.Load<Texture2D>("Models/oger_cook_spritesheet_lowRes");
+        ogerCook = new Player(_ogerCookSpritesheet,
+            new Vector2(midScreenWidth, midScreenHeight), _animationManager); //oger Position 
+            //1f); //ogerSpeed    just for MovingSprite
+    }
 
-        protected override void Initialize()
-        {
-            //to make using calc for middel of Screen shorter 
-            midScreenWidth = _graphics.PreferredBackBufferWidth / 2;
-            midScreenHeight = _graphics.PreferredBackBufferHeight / 2;
+    protected override void Update(GameTime gameTime)
+    {
+        if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
 
-            base.Initialize();
-        }
+        ogerCook.Update();
+        _animationManager.Update();
 
-        protected override void LoadContent()
-        {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        base.Update(gameTime);
+    }
 
-            //constructing new Animation with 16 Frames in 4 Rows 
-            _animationManager = new(4, 4, new Vector2(19, 32));
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Beige);
 
-            //local implementation, cuz accec to texture via Sprite class 
-            Texture2D _ogerCookSpritesheet = Content.Load<Texture2D>("Models/oger_cook_spritesheet_lowRes");
-            ogerCook = new Player(_ogerCookSpritesheet,
-                new Vector2(midScreenWidth, midScreenHeight), _animationManager); //oger Position 
-                //1f); //ogerSpeed    just for MovingSprite
-        }
+        _spriteBatch.Begin(samplerState: SamplerState.PointClamp); //to make sharp images while scaling 
 
-        protected override void Update(GameTime gameTime)
-        {
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+        _spriteBatch.Draw(
+            ogerCook.texture,               //texture 
+            ogerCook.Rect,                  //destinationRectangle
+            _animationManager.GetFrame(),   //sourceRectangle (frame) 
+            Color.White,                    //color
+            0f,                             //rotation 
+            new Vector2(                    //origin -> to place center texture correctly
+                ogerCook.texture.Width/4, 
+                ogerCook.texture.Width/4),         
+            SpriteEffects.None,             //effects
+            0f);                            //layer depth
+        
+        _spriteBatch.End();
 
-            ogerCook.Update();
-            _animationManager.Update();
-
-            base.Update(gameTime);
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.Beige);
-
-            _spriteBatch.Begin(samplerState: SamplerState.PointClamp); //to make sharp images while scaling 
-
-            _spriteBatch.Draw(
-                ogerCook.texture,             //texture 
-                ogerCook.Rect,                //destinationRectangle
-                _animationManager.GetFrame(),   //sourceRectangle (frame) 
-                Color.White,                    //color
-                0f,                             //rotation 
-                new Vector2(                    //origin -> to place center texture correctly
-                    ogerCook.texture.Width/4, 
-                    ogerCook.texture.Width/4),         
-                SpriteEffects.None,             //effects
-                0f);                            //layer depth
-            
-            _spriteBatch.End();
-
-            base.Draw(gameTime);
-        }
+        base.Draw(gameTime);
     }
 }
