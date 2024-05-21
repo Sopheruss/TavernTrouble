e8c55da97132759cb27beb0c9bdcb2e58a7effc6
Sophie <sophie_moosmann@web.de>
2024-05-21 15:09:33 +0200

    Completed Animation Stop
    PROBLEM:
    links und hoch/runter oder rechts und hoch/runter funktioniert nicht

--- SoftwareProjekt2024/Components/Player.cs
+++ SoftwareProjekt2024/Components/Player.cs
@@ -7,6 +7,7 @@ namespace SoftwareProjekt2024.Components;
 
 internal class Player : SpriteClasses.ScaledSprite
 {
+
     AnimationManager _animManager;
     public Player(Texture2D texture, Vector2 position, AnimationManager animationManager) : base(texture, position)
     {
@@ -20,25 +21,26 @@ internal class Player : SpriteClasses.ScaledSprite
         if (Keyboard.GetState().IsKeyDown(Keys.A))
         {
             position.X -= 1;
+            _animManager.PlayAnimation = true; //playes Animation
             _animManager.RowPos = 0; //changes Animation to Left 
-        }
-
-        if (Keyboard.GetState().IsKeyDown(Keys.D))
+        } else if (Keyboard.GetState().IsKeyDown(Keys.D))
         {
             position.X += 1;
+            _animManager.PlayAnimation = true;
             _animManager.RowPos = 1; //changes Animation to right 
-        }
-
-        if (Keyboard.GetState().IsKeyDown(Keys.W))
+        } else if (Keyboard.GetState().IsKeyDown(Keys.W))
         {
             position.Y -= 1;
+            _animManager.PlayAnimation = true;
             _animManager.RowPos = 2; //changes Animation to up
-        }
-
-        if (Keyboard.GetState().IsKeyDown(Keys.S))
+        } else if (Keyboard.GetState().IsKeyDown(Keys.S))
         {
             position.Y += 1;
+            _animManager.PlayAnimation = true;
             _animManager.RowPos = 3; //changes Animation to down 
+        } else
+        {
+            _animManager.PlayAnimation = false; //stopps Animation
         }
     }
 }
