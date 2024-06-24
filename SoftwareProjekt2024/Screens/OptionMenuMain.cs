84f9ce3fcb63581aef28bc3704e40efd9326f845
b1ttersuess <jonasschwab2000@gmail.com>
2024-06-24 14:19:23 +0200

    new font added to contentpipline via monogame.extended importer/exporter

--- SoftwareProjekt2024/Screens/OptionMenuMain.cs
+++ SoftwareProjekt2024/Screens/OptionMenuMain.cs
@@ -2,81 +2,82 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.BitmapFonts;
 using SoftwareProjekt2024.Components;
 
-namespace SoftwareProjekt2024.Screens;
-
-internal class OptionMenuMain
+namespace SoftwareProjekt2024.Screens
 {
-    readonly Game1 _game;
-    readonly SpriteBatch _spriteBatch;
-
-    readonly int midScreenWidth;
-    readonly int midScreenHeight;
-
-    readonly Button _returnButton;
-
-    SpriteFont font;
-
-    public OptionMenuMain(ContentManager Content, int screenWidth, int screenHeight, Game1 game, SpriteBatch spriteBatch)
+    internal class OptionMenuMain
     {
-        _game = game;
-        _spriteBatch = spriteBatch;
+        readonly Game1 _game;
+        readonly SpriteBatch _spriteBatch;
 
-        midScreenWidth = screenWidth / 2;
-        midScreenHeight = screenHeight / 2;
+        readonly int midScreenWidth;
+        readonly int midScreenHeight;
 
+        readonly Button _returnButton;
 
-        _returnButton = new Button(
-            Content.Load<Texture2D>("Buttons/returnButton"),
-            Content.Load<Texture2D>("Buttons/returnButtonHovering"),
-            new Vector2(screenWidth - 70, screenHeight - 70));
+        BitmapFont bmfont;
 
-
-        font = Content.Load<SpriteFont>("File");
-        _spriteBatch = spriteBatch;
-    }
-
-    public void Update()
-    {
-        _returnButton.Update();
-
-
-        if (_returnButton.isClicked || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        public OptionMenuMain(ContentManager Content, int screenWidth, int screenHeight, Game1 game, SpriteBatch spriteBatch)
         {
-            _game.activeScene = Scenes.MAINMENU;
-        }
-    }
+            _game = game;
+            _spriteBatch = spriteBatch;
 
-    public void Draw()
-    {
-        _spriteBatch.Begin(samplerState: SamplerState.PointClamp); //to make sharp images while scaling 
+            midScreenWidth = screenWidth / 2;
+            midScreenHeight = screenHeight / 2;
 
-        _returnButton.Draw(_spriteBatch);
+            _returnButton = new Button(
+                Content.Load<Texture2D>("Buttons/returnButton"),
+                Content.Load<Texture2D>("Buttons/returnButtonHovering"),
+                new Vector2(screenWidth - 70, screenHeight - 70));
 
-     // Placeholder, how to draw "fonts"
-
-        string[] keys = { "W", "A", "S", "D", "E", "Esc" };
-        string[] descriptions = {
-                "Move Up",
-                "Move Left",
-                "Move Down",
-                "Move Right",
-                "Interact",
-                "Pause/Menu"
-            };
-
-        Vector2 position = new Vector2(midScreenWidth -150 , midScreenHeight - 150); // Starting position for the table
-        float lineHeight = font.LineSpacing + 5; // Line height with a small gap
-
-        for (int i = 0; i < keys.Length; i++)
-        {
-            _spriteBatch.DrawString(font, keys[i], position, Color.White);
-            _spriteBatch.DrawString(font, descriptions[i], new Vector2(position.X + 150, position.Y), Color.Black);
-            position.Y += lineHeight; // Move down for the next line
-            
+            bmfont = Content.Load<BitmapFont>("Fonts/font_new");
         }
 
-     _spriteBatch.End();
+        public void Update()
+        {
+            _returnButton.Update();
+
+            if (_returnButton.isClicked || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                _game.activeScene = Scenes.MAINMENU;
+            }
+        }
+
+        public void Draw()
+        {
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp); // to make sharp images while scaling
+
+            _returnButton.Draw(_spriteBatch);
+
+            // Ensure bmfont is not null before drawing
+            if (bmfont != null)
+            {
+
+                // Add the key bindings display
+                string[] keys = { "W", "A", "S", "D", "E", "Esc" };
+                string[] descriptions = {
+                    "Move Up",
+                    "Move Left",
+                    "Move Down",
+                    "Move Right",
+                    "Interact",
+                    "Pause/Menu"
+                };
+
+                Vector2 position = new Vector2(midScreenWidth - 150, midScreenHeight - 150); // Starting position for the table
+                float lineHeight = bmfont.LineHeight + 5; // Line height with a small gap
+
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    _spriteBatch.DrawString(bmfont, keys[i], position, Color.White);
+                    _spriteBatch.DrawString(bmfont, descriptions[i], new Vector2(position.X + 150, position.Y), Color.Black);
+                    position.Y += lineHeight; // Move down for the next line
+                }
+            }
+
+            _spriteBatch.End();
+        }
     }
 }
