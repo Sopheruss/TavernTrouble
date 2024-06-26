ac32994beb3c4250940af744e8c45b17f4182309
timnowk <167316153+timnowk@users.noreply.github.com>
2024-06-26 17:29:10 +0200

    new interaction logic, changes in InteractionManager and InputManager, needs interactionLayer and handle-logic to do sth

--- SoftwareProjekt2024/Managers/InteractionManager.cs
+++ SoftwareProjekt2024/Managers/InteractionManager.cs
@@ -14,6 +14,10 @@ namespace SoftwareProjekt2024
     {
         TileManager _tileManager;
         CollisionManager _collisionManager;
+
+        readonly int quarterTileHeight = 8;
+        readonly int tileSize = 32;
+
         List<Rectangle> intersections;
 
         public InteractionManager(TileManager tilemanager)
@@ -21,5 +25,39 @@ namespace SoftwareProjekt2024
             _tileManager = tilemanager;
         }
 
+        public int CheckInteraction(Rectangle bounds)
+            // uses collision logic, could also return Strings if preferred
+        {
+            foreach (var tile in _tileManager.interactionLayer)
+            {
+                Rectangle tileRect;
+
+                switch ((int)tile.Value) 
+                {
+                    // cases: if we want to draw rects in different sizes for specific tiles
+                    // default: normal rects in tile-size
+
+                    default:
+                        tileRect = new Rectangle((int)tile.Key.X * 32, (int)tile.Key.Y * 32, 32, 32);
+                        break;
+                }
+
+                if (tileRect.Intersects(bounds))
+                {
+                    return (int)tile.Value; // returns tile ID of intersecting rect to handle interaction for different tile-types later; true
+                }
+            }
+            return 0; // 0 means no possible interaction; false
+        }
+
+        public void HandleInteraction(int tileID)
+        {
+            switch(tileID)
+            {
+                default:
+                    Debug.WriteLine("INTERACTION");
+                    break;
+            }
+        }
     }
 }
