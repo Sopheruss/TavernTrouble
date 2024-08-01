8ce6224292c31e01da8cc59a77632b7d01c05c5d
Sophie <sophie_moosmann@web.de>
2024-08-01 09:51:34 +0200

    deleting all of the old stuff for easier merge

--- SoftwareProjekt2024/Managers/CollisionManager.cs
+++ SoftwareProjekt2024/Managers/CollisionManager.cs
@@ -1,12 +1,1 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
-using Microsoft.Xna.Framework;
-using MonoGame.Extended.Tiled;
-
-namespace SoftwareProjekt2024
-{
-    public class CollisionManager
-    {   
-    }
-}
-
+﻿
--- SoftwareProjekt2024/Screens/Screen.cs
+++ SoftwareProjekt2024/Screens/Screen.cs
@@ -1,11 +1,1 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-
-namespace SoftwareProjekt2024.Screens;
-
-public interface IScreen
-{
-    public void Load();
-    public void Update(GameTime gameTime);
-    public void Draw(SpriteBatch spriteBatch);
-};
+﻿
