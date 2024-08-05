017511a9314208efc4fb99b42800170ba54b57ec
Sophie <sophie_moosmann@web.de>
2024-08-05 12:35:20 +0200

    sorting menu, changing positions, begining volume

--- SoftwareProjekt2024/Screens/OptionMenuMain.cs
+++ SoftwareProjekt2024/Screens/OptionMenuMain.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.BitmapFonts;
 using SoftwareProjekt2024.Components;
 
 namespace SoftwareProjekt2024.Screens;
@@ -10,47 +11,84 @@ internal class OptionMenuMain
     readonly Game1 _game;
     readonly SpriteBatch _spriteBatch;
 
-    readonly int midScreenWidth;
-    readonly int midScreenHeight;
+    readonly int _screenWidth;
+    readonly int _screenHeight;
+    readonly int _midScreenWidth;
+    readonly int _midScreenHeight;
 
     private MouseState _currentMouse;
     private MouseState _previousMouse;
 
     readonly Button _returnButton;
 
-    Texture2D _controls;
-    Rectangle _controlsRect;
+    readonly int _edgeSpacer;
 
-    Texture2D _fullScreenOn;
-    Texture2D _fullScreenOff;
-    Rectangle _fullScreenRect;
+    readonly string _controls;
+    readonly Texture2D _controlsTexture;
+    readonly Rectangle _controlsRect;
+    readonly Vector2 _controlsTextSize;
+
+    readonly string _fullScreen;
+    readonly string _fullScreenOnText;
+    readonly Vector2 _onSize;
+    readonly Vector2 _offSize;
+    readonly string _fullScreenOffText;
+    readonly Vector2 _fullScreenTextSize;
+    readonly Texture2D _fullScreenOn;
+    readonly Texture2D _fullScreenOff;
+    readonly Rectangle _fullScreenRect;
 
     bool _fullIsClicked;
 
-    //BitmapFont bmfont;
+    readonly string _volume;
+    readonly Vector2 _volumeTextSize;
+    readonly Texture2D _volumeBarTexture;
+    readonly Rectangle _volumeBarRect;
+    readonly Texture2D _volumeButton;
+    readonly Rectangle _volumeButtonRect;
+
+    readonly BitmapFont bmfont;
 
     public OptionMenuMain(ContentManager Content, int screenWidth, int screenHeight, Game1 game, SpriteBatch spriteBatch)
     {
         _game = game;
         _spriteBatch = spriteBatch;
 
-        midScreenWidth = screenWidth / 2;
-        midScreenHeight = screenHeight / 2;
+        _screenWidth = screenWidth;
+        _screenHeight = screenHeight;
+        _midScreenWidth = screenWidth / 2;
+        _midScreenHeight = screenHeight / 2;
 
         _returnButton = new Button(
             Content.Load<Texture2D>("Buttons/returnButton"),
             Content.Load<Texture2D>("Buttons/returnButtonHovering"),
             new Vector2(screenWidth - 70, screenHeight - 70));
 
-        _controls = Content.Load<Texture2D>("OptionMenu/controls");
-        _controlsRect = new Rectangle((screenWidth / 2) - 250, (screenHeight / 2) - 140, _controls.Width * 4, _controls.Height * 4);
+        _edgeSpacer = 50;
 
+        _controls = "Controls:";
+        _controlsTexture = Content.Load<Texture2D>("OptionMenu/controls");
+        int controlsTextureWidth = _controlsTexture.Width * 3;
+        int controlsTextureHeight = _controlsTexture.Height * 3;
+        _controlsRect = new Rectangle(_edgeSpacer, _midScreenHeight - controlsTextureHeight / 2, controlsTextureWidth, controlsTextureHeight);
+
+        _fullScreen = "Fullscreen:";
+        _fullScreenOffText = "Off";
+        _fullScreenOnText = "On";
         _fullScreenOn = Content.Load<Texture2D>("Buttons/fullScreenButtonOn"); //not hovering = on
         _fullScreenOff = Content.Load<Texture2D>("Buttons/fullScreenButtonOff"); //hovering = off
-        _fullScreenRect = new Rectangle((screenWidth / 2) - 40, screenHeight - 150, _fullScreenOn.Width, _fullScreenOn.Height);
+
+        _volume = "Volume:";
 
         // texture as well as fnt file have to be imported via content-pipline and monogame.extended importer. Beware of filestructure
-        //bmfont = Content.Load<BitmapFont>("Fonts/font_new");
+        bmfont = Content.Load<BitmapFont>("Fonts/font_new");
+        _fullScreenTextSize = bmfont.MeasureString(_fullScreen);
+        _controlsTextSize = bmfont.MeasureString(_controls);
+        _onSize = bmfont.MeasureString(_fullScreenOnText);
+        _offSize = bmfont.MeasureString(_fullScreenOffText);
+        _volumeTextSize = bmfont.MeasureString(_volume);
+
+        _fullScreenRect = new Rectangle(_midScreenWidth + _edgeSpacer + (int)_offSize.X + 20, 245, _fullScreenOn.Width, _fullScreenOn.Height);
     }
 
     public void Update()
@@ -89,9 +127,15 @@ internal class OptionMenuMain
 
         _returnButton.Draw(_spriteBatch);
 
+        _spriteBatch.DrawString(bmfont, _fullScreen, new Vector2(_midScreenWidth + _edgeSpacer, 200), Color.Black);
+        _spriteBatch.DrawString(bmfont, _fullScreenOffText, new Vector2(_midScreenWidth + _edgeSpacer, 250), Color.Black);
+        _spriteBatch.DrawString(bmfont, _fullScreenOnText, new Vector2(_midScreenWidth + _edgeSpacer + _offSize.Y + 50 + _fullScreenRect.Width, 250), Color.Black);
         _spriteBatch.Draw(_fullScreenOn, _fullScreenRect, Color.White);
 
-        _spriteBatch.Draw(_controls, _controlsRect, Color.White);
+        _spriteBatch.DrawString(bmfont, _volume, new Vector2(_midScreenWidth + _edgeSpacer, _midScreenHeight), Color.Black);
+
+        _spriteBatch.DrawString(bmfont, _controls, new Vector2(_edgeSpacer, 200), Color.Black);
+        _spriteBatch.Draw(_controlsTexture, _controlsRect, Color.White);
 
 
         if (_game.fullScreen && _fullIsClicked)
@@ -113,33 +157,6 @@ internal class OptionMenuMain
             _spriteBatch.Draw(_fullScreenOff, _fullScreenRect, Color.White);
 
         }
-
-
-        /*// Ensure bmfont is not null before drawing
-        if (bmfont != null)
-        {
-
-            // Add the key bindings display
-            string[] keys = { "W", "A", "S", "D", "E", "Esc" };
-            string[] descriptions = {
-                "Move Up",
-                "Move Left",
-                "Move Down",
-                "Move Right",
-                "Interact",
-                "Pause/Menu"
-            };
-
-            Vector2 position = new Vector2(midScreenWidth - 150, midScreenHeight - 150); // Starting position for the table
-            float lineHeight = bmfont.LineHeight + 5; // Line height with a small gap
-
-            for (int i = 0; i < keys.Length; i++)
-            {
-                _spriteBatch.DrawString(bmfont, keys[i], position, Color.White);
-                _spriteBatch.DrawString(bmfont, descriptions[i], new Vector2(position.X + 150, position.Y), Color.Black);
-                position.Y += lineHeight; // Move down for the next line
-            }
-        }*/
 
         _spriteBatch.End();
     }
