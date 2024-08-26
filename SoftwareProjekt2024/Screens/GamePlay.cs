de91695271483bd7f334dfb853b25eb9ee732925
timnowk <167316153+timnowk@users.noreply.github.com>
2024-08-26 11:33:04 +0200

    centered textline

--- SoftwareProjekt2024/Screens/GamePlay.cs
+++ SoftwareProjekt2024/Screens/GamePlay.cs
@@ -327,7 +327,8 @@ public class GamePlay
 
         if (_showPossibleInteraction)
         {
-            _spriteBatch.DrawString(bmfont, "Press E to interact with " + _possibleInteractionObject, new Vector2(_screenWidth / 2 - (int)_keyPressLetterSize.X / 2, _screenHeight - 15 - (int)_keyPressLetterSize.Y), Color.Beige);
+            Vector2 textSize = bmfont.MeasureString("Press E to interact with " + _possibleInteractionObject);
+            _spriteBatch.DrawString(bmfont, "Press E to interact with " + _possibleInteractionObject, new Vector2((_screenWidth - textSize.X) / 2, _screenHeight - 15 - (int)_keyPressLetterSize.Y), Color.Beige);
         }
 
         _spriteBatch.End();
