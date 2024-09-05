e3229f56799417ba417948ed8403809516757753
Leon Brunke <lanceplayer15@gmail.com>
2024-09-05 23:03:12 +0200

    a

--- SoftwareProjekt2024/Components/DekoObjects/CandleOnTable.cs
+++ SoftwareProjekt2024/Components/DekoObjects/CandleOnTable.cs
@@ -8,5 +8,5 @@ internal class CandleOnTable : StaticObject
 {
     public CandleOnTable(Texture2D texture, Vector2 position, Rectangle _dest, Rectangle _src, PerspectiveManager perspectiveManager)
         : base(texture, position, _dest, _src, perspectiveManager)
-    { }
+    { }//
 }
